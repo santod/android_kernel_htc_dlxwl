84c958a10505e55bf21c4782fbc5e5e5229c1728
santod <santod040@gmail.com>
2014-04-25 23:42:19 -0700

    initial commit and fix-up of HTC sources

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Apr 25 23:24:22 PDT 2014"
+#define LINUX_COMPILE_BY "santod"
+#define LINUX_COMPILE_HOST "DevBoX"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
